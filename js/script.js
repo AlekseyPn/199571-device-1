36177af04f395e8010cf1b1a2197953dcbecac13
AleksePn <aleksfers@icloud.com>
2016-11-11 23:02:09 +0500

    bug fix

--- js/script.js
+++ js/script.js
@@ -7,25 +7,25 @@ var mapOpen = document.querySelector(".company__map");
 var mapClose = document.querySelector(".modal-map__close");
 var overlay = document.querySelector(".modal-overlay");
 
-function openModal(click, popupVar) {
+function openModal(click, popupVar, overlayVar) {
     click.addEventListener("click", function(event) {
         event.preventDefault();
         popupVar.classList.add("modal-contents--show");
-        overlay.classList.add("modal-overlay--show");
+        overlayVar.classList.add("modal-overlay--show");
         popup.scrollIntoView(top);
     })
 };
 
-function closeModal(click, popupVar) {
+function closeModal(click, popupVar, overlayVar) {
     click.addEventListener("click", function(event) {
         event.preventDefault();
         popupVar.classList.remove("modal-contents--show");
-        overlay.classList.remove("modal-overlay--show");
+        overlayVar.classList.remove("modal-overlay--show");
     })
 };
 
-openModal(popupOpen, popup);
-openModal(mapOpen, popupMap);
-closeModal(popupClose, popup);
-closeModal(overlay, popup);
-closeModal(mapClose, popupMap);
+openModal(popupOpen, popup, overlay);
+openModal(mapOpen, popupMap, overlay);
+closeModal(popupClose, popup, overlay);
+closeModal(overlay, popup, overlay);
+closeModal(mapClose, popupMap, overlay);
